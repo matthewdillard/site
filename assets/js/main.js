fffa7801ec692f071aa1c836acf587804dfb56cd
Matthew Dillard <67643900+matthewdillard@users.noreply.github.com>
2026-02-06 15:29:34 -0500

    feat: add Gold Post font, responsive text sizing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
 // Listen for sticky note interaction
 document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(adjustStickySizing, 100);
+    setTimeout(adjustPostItSizing, 100);
+    
+    var stickyInput = document.getElementById("postit-input");
+    
+    stickyInput.addEventListener("input", adjustPostItSizing);
+    window.addEventListener("resize", adjustPostItSizing);
+    
+    function adjustPostItSizing() {
+        var maxHeight = parseInt(getComputedStyle(stickyInput.parentElement).height) * .97;
+        var fontSize = maxHeight / 7.75;
+        
+        stickyInput.style.height = maxHeight + "px";
+        stickyInput.style.fontSize = fontSize + "px";
+        console.log("adjusting sticky size to ", fontSize, " px");
+    }
 });
-
-var stickyInput = document.getElementById("sticky-input");
-
-stickyInput.addEventListener("input", adjustStickySizing);
-window.addEventListener("resize", adjustStickySizing);
-
-function adjustStickySizing() {
-    var maxHeight = parseInt(getComputedStyle(stickyInput.parentElement).height) * .97;
-    var fontSize = maxHeight / 7.75;
-
-    stickyInput.style.height = maxHeight + "px";
-    stickyInput.style.fontSize = fontSize + "px";
-}
